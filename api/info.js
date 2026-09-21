97700b9a69c2f32ed1a2b19cfd7d91893e2318ad
Masean24 <ramadhandaris24@gmail.com>
2026-09-22 00:15:44 +0700

    fix(api): use raw Node.js response methods in info.js for Vercel compatibility

--- api/info.js
+++ api/info.js
@@ -4,15 +4,18 @@ module.exports = (req, res) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') {
-    return res.status(204).end();
+    res.statusCode = 204;
+    return res.end();
   }
 
   const host = req.headers.host || 'localhost';
   const proto = req.headers['x-forwarded-proto'] || 'https';
 
-  res.status(200).json({
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({
     localIP: host,
     httpUrl: `${proto}://${host}`,
     httpsUrl: `${proto}://${host}`
-  });
+  }));
 };
